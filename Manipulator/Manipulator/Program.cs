2edad206af24a5637e23a6d392570e0536dd5cf1
martushoka <martushoka.10@gmail.com>
2022-02-14 10:48:16 +0200

    Manipulator

---
+++ Manipulator/Manipulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
