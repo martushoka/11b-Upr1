bbc55b31f49bb7d4eb2bb6d7a75fa00a1e5c9345
martushoka <martushoka.10@gmail.com>
2022-02-15 12:59:12 +0200

    Spisuci

--- Manipulator/Manipulator/Program.cs
+++ Manipulator/Manipulator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Manipulator
 {
@@ -6,7 +8,60 @@ namespace Manipulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                if (input[0].ToLower() == "add")
+                {
+                    if (!numbers.Contains(int.Parse(input[2])))
+
+                    {
+                        numbers.Insert(int.Parse(input[1]), int.Parse(input[2]));
+                    }
+                }
+
+                if (input[0].ToLower() == "contains")
+                {
+                    if (numbers.Contains(int.Parse(input[1])))
+
+                    {
+                        Console.WriteLine(numbers.IndexOf(int.Parse(input[1])));
+                    }
+                    else
+                    {
+                        Console.WriteLine(-1);
+                    }
+                }
+
+                if (input[0].ToLower() == "swap")
+                {
+                    int element1 = numbers.IndexOf(int.Parse(input[1]));
+                    int element2 = numbers.IndexOf(int.Parse(input[2]));
+
+                    int temp = numbers[element1];
+                    numbers[element1] = numbers[element2];
+                    numbers[element2] = temp;
+
+                }
+
+                if (input[0].ToLower() == "remove")
+                {
+                    if (int.Parse(input[1]) >=0)
+                    {
+                        numbers.RemoveAt(int.Parse(input[1]));
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("")
+                    }
+                }
+            }
         }
     }
 }
---
+++ Spisuchen manipulator/Spisuchen manipulator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Spisuchen_manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "print")
+                {
+                    Console.WriteLine(string.Join(" ", numbers));
+                    break;
+                }
+                if (int.TryParse(input,out int n) == true)
+                {
+                    numbers.Sort();
+
+                    Console.WriteLine(string.Join(" ", numbers));
+                }
+
+                string[] commands = input.Split();
+
+                switch (commands[0])
+                {
+                    case "add":
+                        {
+                            int element = int.Parse(commands[1]);
+                            int index = int.Parse(commands[2]);
+
+                            
+                            if (!numbers.Contains(element))
+                            {
+                                numbers.Insert(element, index);
+                            }
+
+                            break;
+                        }
+
+                    case "contains":
+                        {
+                            int numberToSearch = int.Parse(commands[1]);
+                            if (numbers.Contains(numberToSearch))
+                            {
+                                Console.WriteLine(numbers.IndexOf(numberToSearch));
+                            }
+                            else
+                            {
+                                Console.WriteLine("Sasho", "Petar");
+                            }
+                            break;
+                        }
+                }
+
+
+            }
+        }
+
+    }
+}
