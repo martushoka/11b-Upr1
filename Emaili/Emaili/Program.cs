eede7be8e7607c7867a0a8a493bd950eb6313ad2
martushoka <martushoka.10@gmail.com>
2022-02-28 10:42:36 +0200

    Emails

---
+++ Emaili/Emaili/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Emaili
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> emailList = new Dictionary<string, string>();
+
+            string[] input = Console.ReadLine().Split();
+
+            while (true)
+            {
+                if (input[0] == "Stop")
+                {
+                    break;
+                }
+
+
+
+
+
+                if (input[0].ToLower() == "Add")
+                {
+                    if (!emailList.ContainsKey(input[1]))
+                    {
+                        emailList.Add(input[1], input[2]);
+                    }
+
+                    else
+                    {
+                        emailList[input[1]] = input[2];
+                    }
+                }
+
+                if (input[0] == "Sent")
+                {
+                    if (emailList.ContainsKey(input[1]))
+                    {
+                        Console.WriteLine($"{input[1]} -> {emailList[input[1]]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {input[1]} does not exist");
+                    }
+                }
+
+                input = Console.ReadLine().Split();
+            }
+
+            foreach ( var name in emailList)
+            {
+                Console.WriteLine($"{name.Key} -> {name.Value}");
+            }
+            
+        }
+    }
+}
